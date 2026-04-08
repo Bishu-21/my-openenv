7fb34f6c38daa570f4873bc820279bb3b3a69e54
Bishu-21 <sbishal947.official@gmail.com>
2026-04-09 00:11:06 +0530

    Add server main guard

--- server/app.py
+++ server/app.py
@@ -10,3 +10,7 @@ from app.main import app
 def main() -> None:
     port = int(os.getenv("PORT", "7860"))
     uvicorn.run("server.app:app", host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    main()
